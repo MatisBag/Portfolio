a1ad5ff9113af4e1d9063ebdbb085b527581c217
Matis Baguelin <matis.baguelin@gmail.com>
2020-04-05 18:31:06 +0200

    update dark mode 2nd page

---
+++ espace_administrateur/script.js
@@ -0,0 +1,26 @@
+const darklightmode = localStorage.getItem("darklightmode");
+
+if (darklightmode == "dark")
+{
+        const body = document.querySelector("body");
+    
+        toggleDarkLightForElement(body, 'light-mode', 'dark-mode')
+    
+        const solidBorders = document.querySelectorAll('.solid-border'); 
+        
+        solidBorders.forEach((solidBorder) => {
+            toggleDarkLightForElement(solidBorder, 'light-mode', 'dark-mode')
+        })
+    
+    function toggleDarkLightForElement(elem, lighModeClassname, darkModeClassname) {
+        if (elem.classList.contains(lighModeClassname)) {
+            elem.classList.remove(lighModeClassname)
+            elem.classList.add(darkModeClassname);
+        } else if (elem.classList.contains(darkModeClassname)) {
+            elem.classList.remove(darkModeClassname)
+            elem.classList.add(lighModeClassname);
+        } else {
+            elem.classList.add(darkModeClassname)
+        }
+    }
+}
